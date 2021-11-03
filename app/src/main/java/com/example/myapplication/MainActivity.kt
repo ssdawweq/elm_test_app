189f7015fdd2c99685208aa22f1c741abdcdfbdf
unknown <a.almajed@lean.sa>
2021-11-03 21:17:21 +0300

    main
    init set up

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
