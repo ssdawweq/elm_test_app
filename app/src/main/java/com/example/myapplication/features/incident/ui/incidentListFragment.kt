c543262fecf28c9c27984128d0f2dc000388fb41
unknown <a.almajed@lean.sa>
2021-11-03 22:11:31 +0300

    incident
    Added:
     - fragment infaltion with view binding of incident list fragment
     - Added to the nav graph as the first start for now

---
+++ app/src/main/java/com/example/myapplication/features/incident/ui/incidentListFragment.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication.features.incident.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentIncidentListBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class IncidentListFragment: Fragment() {
+
+    private var _binding: FragmentIncidentListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentIncidentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
